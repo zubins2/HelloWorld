618d17fe7788884f166b64d49c7aae577937f8ce
ZubinSundaram <zubins2@nycstudents.net>
2025-09-09 15:15:26 -0400

    added user input and a print statement

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,10 @@
 package PACKAGE_NAME;
 
 public class Main {
-}
+    public static void main(String[] args)
+    {
+        System.out.println("What's your name?");
+        Scanner s = new Scanner(System.in);
+        String name = s.nextLine();
+        System.out.println("Hello, " + name + "!");
+    }
