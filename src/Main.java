02a1263d1f2b1ca1826d70c668f1ca9d0d0976c0
ZubinSundaram <zubins2@nycstudents.net>
2025-09-09 15:31:05 -0400

    added a file. added a new line which uses the file to give an output to the user.

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,15 @@
 import java.util.Scanner;
+import java.io.File;
+import java.io.FileNotFoundException;
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException {
         System.out.println("What's your name?");
         Scanner s = new Scanner(System.in);
         String name = s.nextLine();
         System.out.println("Hello, " + name + "!");
+        File f = new File("Class.txt");
+        s = new Scanner(f);
+        String course = s.nextLine();
+        System.out.println("Welcome to " + course + "!");
     }
 }
