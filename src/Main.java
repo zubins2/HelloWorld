9ea7b6b37023c1a07c0e02e6f0aceeeae9e10d4c
ZubinSundaram <zubins2@nycstudents.net>
2025-09-09 15:16:22 -0400

    fixed small error

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,9 @@
-package PACKAGE_NAME;
-
+import java.util.Scanner;
 public class Main {
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) {
         System.out.println("What's your name?");
         Scanner s = new Scanner(System.in);
         String name = s.nextLine();
         System.out.println("Hello, " + name + "!");
     }
+}
